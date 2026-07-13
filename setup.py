4fc835a11c233ff60dff40e4d94e201c9ad55f60
personal-portfolio <aayushpkamble2006@gmail.com>
2026-07-13 14:19:28 +0530

    exceptions

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
-from setuptools import find_packages , setup
+from setuptools import find_packages, setup
 from typing import List
 
 HYPEN_E_DOT = "-e ."
 
-def get_requirements(file_path:str)-> List[str]:
+
+def get_requirements(file_path: str) -> List[str]:
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements =  [req.replace("/n" , "") for req in requirements] 
+        requirements = [req.replace("\n", "") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-        
+
     return requirements
 
-setup(
-    name = "ml_project",
-    version = "0.0.1",
-    author = "Aayush",
-    authoe_email ="aayushpkamble2006@gmail.com",
-    packages=find_packages(),
-    requires = get_requirements("requirements.txt")
 
+setup(
+    name="ml_project",
+    version="0.0.1",
+    author="Aayush",
+    author_email="aayushpkamble2006@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
 )
---
+++ src/exception.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def error_detail_exception(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    filename = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = "Error occurred in file [{0}] at line [{1}]: {2}".format(
+        filename, line_number, str(error)
+    )
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail):
+        super().__init__(error_message)
+        self.error_message = error_detail_exception(error_message, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
